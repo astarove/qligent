c0215fbce388194ccf06e6285603213b4cfcbb91
Anton Staroverov <avstaroverov@gmail.com>
2017-05-21 08:26:55 +0300

    Add custom field 'configuration'

--- mantisbt/bug_report_page.php
+++ mantisbt/bug_report_page.php
@@ -543,6 +543,43 @@ if( $t_show_attachments ) {
 			<input <?php echo helper_get_tab_index() ?> type="text" id="summary" name="summary" size="105" maxlength="128" value="<?php echo string_attribute( $f_summary ) ?>" />
 		</td>
 	</tr>
+<?php
+    # Custom field configuration
+	$t_custom_fields_found = false;
+	$t_related_custom_field_ids = custom_field_get_linked_ids( $t_project_id );
+
+	foreach( $t_related_custom_field_ids as $t_id ) {
+		$t_def = custom_field_get_definition( $t_id );
+ 		if( $t_def['name'] == 'configuration' ) {
+            if( ( $t_def['display_report'] || $t_def['require_report']) && custom_field_has_write_access_to_project( $t_id, $t_project_id ) ) {
+                $t_custom_fields_found = true;
+
+                $t_required_class = $t_def['require_report'] ? 'class="required" ' : '';
+
+                if( $t_def['type'] != CUSTOM_FIELD_TYPE_RADIO && $t_def['type'] != CUSTOM_FIELD_TYPE_CHECKBOX ) {
+                    $t_label_for = 'for="custom_field_' . string_attribute( $t_def['id'] ) . '" ';
+                } else {
+                    $t_label_for = '';
+                }
+?>
+	<tr>
+		<th class="category">
+			<?php if( $t_def['require_report'] ) {?><span class="required">*</span><?php } ?>
+			<?php if( $t_def['type'] != CUSTOM_FIELD_TYPE_RADIO && $t_def['type'] != CUSTOM_FIELD_TYPE_CHECKBOX ) { ?>
+				<label for="custom_field_<?php echo string_attribute( $t_def['id'] ) ?>">
+					<?php echo string_display( lang_get_defaulted( $t_def['name'] ) ) ?>
+				</label>
+			<?php } else { echo string_display( lang_get_defaulted( $t_def['name'] ) ); } ?>
+		</th>
+		<td>
+			<?php print_custom_field_input( $t_def, ( $f_master_bug_id === 0 ) ? null : $f_master_bug_id ) ?>
+		</td>
+	</tr>
+<?php
+            }
+        }
+    }
+?>
 	<tr>
 		<th class="category">
 			<span class="required">*</span><label for="description"><?php print_documentation_link( 'description' ) ?></label>
@@ -590,16 +627,17 @@ if( $t_show_attachments ) {
 
 	foreach( $t_related_custom_field_ids as $t_id ) {
 		$t_def = custom_field_get_definition( $t_id );
-		if( ( $t_def['display_report'] || $t_def['require_report']) && custom_field_has_write_access_to_project( $t_id, $t_project_id ) ) {
-			$t_custom_fields_found = true;
+		if( $t_def['name'] != 'configuration' ) {
+            if( ( $t_def['display_report'] || $t_def['require_report']) && custom_field_has_write_access_to_project( $t_id, $t_project_id ) ) {
+                $t_custom_fields_found = true;
 
-			$t_required_class = $t_def['require_report'] ? 'class="required" ' : '';
+                $t_required_class = $t_def['require_report'] ? 'class="required" ' : '';
 
-			if( $t_def['type'] != CUSTOM_FIELD_TYPE_RADIO && $t_def['type'] != CUSTOM_FIELD_TYPE_CHECKBOX ) {
-				$t_label_for = 'for="custom_field_' . string_attribute( $t_def['id'] ) . '" ';
-			} else {
-				$t_label_for = '';
-			}
+                if( $t_def['type'] != CUSTOM_FIELD_TYPE_RADIO && $t_def['type'] != CUSTOM_FIELD_TYPE_CHECKBOX ) {
+                    $t_label_for = 'for="custom_field_' . string_attribute( $t_def['id'] ) . '" ';
+                } else {
+                    $t_label_for = '';
+                }
 ?>
 	<tr>
 		<th class="category">
@@ -615,6 +653,7 @@ if( $t_show_attachments ) {
 		</td>
 	</tr>
 <?php
+            }
 		}
 	} # foreach( $t_related_custom_field_ids as $t_id )
 ?>
